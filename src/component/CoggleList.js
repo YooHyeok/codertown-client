5f5d67d1e348f907fce26d59649c57a575e178e6
webdevyoo <yjk7454@naver.com>
2023-08-14 19:01:52 +0900

    MOD: Input태그 포커스 제거 - css  boxShadow: none

--- src/component/CoggleList.js
+++ src/component/CoggleList.js
@@ -51,7 +51,7 @@ export default function CoggleList() {
                     <div style={{width:"894px"}}>
                         <FormGroup style={{float:"right", paddingTop: "40px"}}>
                             <InputGroup size="s">
-                                <Input type="text" onKeyDown={(e)=>{}} onChange={{}} placeholder='검색어를 입력하세요' style={{width:"200px", display: "inline-block"}} />
+                                <Input type="text" onKeyDown={(e)=>{}} onChange={{}} placeholder='검색어를 입력하세요' style={{boxShadow: 'none', width:"200px", display: "inline-block"}} />
                                 <Button outline className="d-flex align-items-center" onClick={(e)=>{}} color="secondary" style={{width:"38px", border:"0.1px solid lightgray"}}>
                                     <Search className="ml-auto" style={{margin: '0 -3px 0 -2px', fontSize: '1.5rem' }}/>
                                 </Button>
