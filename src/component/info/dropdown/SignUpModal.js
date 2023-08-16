a8c41720c62c01c1cd6144fa9fd7306f5e092ba7
webdevyoo <yjk7454@naver.com>
2023-08-16 16:31:45 +0900

    MOD: 회원가입 모달 CSS 수정

--- src/component/info/dropdown/SignUpModal.js
+++ src/component/info/dropdown/SignUpModal.js
@@ -17,20 +17,27 @@ export default function SignUpModal() {
         <div>
             <Modal isOpen={context.signUpShow} toggle={context.signUpToggle} style={modalStyle}>
             <ModalHeader toggle={context.signUpToggle} >
-                <span style={{textAlign:"center !important"}}>회원가입</span>
+                <span style={{textAlign:"center !important"}}>간편 회원가입</span>
                 </ModalHeader>
             <ModalBody>
-                <div style={{margin:"20px auto", paddingLeft:"30px"}}>
-                    <FormGroup row style={{paddingLeft:"20px"}}>
-                        <Label htmlFor='email' sm={2}>이&nbsp;&nbsp;메&nbsp;&nbsp;일</Label>
-                        <Col sm={8}>
-                            <Input type='text' name='email' id='email' />
+                <div style={{margin:"20px auto", paddingLeft:"20px"}}>
+                    <FormGroup row >
+                        <Label style={{width:"95px"}} htmlFor='email' sm={2}>이&nbsp;&nbsp;메&nbsp;&nbsp;일</Label>
+                        <Col sm={8} style={{display:"flex"}}>
+                            <Input type='text' name='email' id='email' style={{width:"213px"}}/>&nbsp;
+                            <Button outline color='secondary' className="CheckBtn" onClick={(e)=>{e.preventDefault();}}>중복 확인</Button>
                         </Col>
                     </FormGroup>
-                    <FormGroup row style={{paddingLeft:"20px"}}>
-                        <Label htmlFor='password' sm={2}>패스워드</Label>
+                    <FormGroup row style={{paddingLeft:"0px"}}>
+                        <Label style={{width:"95px"}} htmlFor='password' sm={2}>패스워드</Label>
                         <Col sm={8}>
                             <Input type='text' name='password' id='password' />
+                        </Col>
+                    </FormGroup>
+                    <FormGroup row style={{paddingLeft:"0px"}}>
+                        <Label style={{width:"95px"}} htmlFor='password2' sm={2}>패스워드 확인</Label>
+                        <Col sm={8}>
+                            <Input type='text' name='password2' id='password' />
                         </Col>
                     </FormGroup>
                 </div>
