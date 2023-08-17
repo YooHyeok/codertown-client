881cdae2fded85fb37ef641546278ad9f11849f7
webdevyoo <yjk7454@naver.com>
2023-08-17 10:59:29 +0900

    MOD: 기존 회원가입 모달을 중간 분기 모달로 수정

--- src/component/info/dropdown/SignUpModal.js
+++ src/component/info/dropdown/SignUpModal.js
@@ -16,53 +16,28 @@ export default function SignUpModal() {
     return(
             <Modal isOpen={context.signUpShow} toggle={context.loginShow == true ? context.signInterToggle : context.signUpToggle} style={modalStyle}>
                 <ModalHeader toggle={context.loginShow == true ? context.signInterToggle : context.signUpToggle} >
-                    <span style={{textAlign:"center !important"}}>간편 회원가입</span>
+                    <span style={{textAlign:"center !important"}}>회원가입</span>
                 </ModalHeader>
                 <ModalBody>
-                    <Form >
-                        <FormGroup >
-                            <Row>
-                                <Col sm={12} >
-                                    <Label htmlFor='email' sm={2}>이메일</Label>
-                                    <span>&#10004; 사용가능</span>
-                                    <span>&#10060; 사용불가</span>
-                                    <Button style={{float:"right"}} outline color='secondary' size="sm" className="existsBtn" onClick={(e)=>{e.preventDefault();}}>중복 확인</Button>
-                                    <Input type='text' name='email' id='email' />
-                                </Col>
-                            </Row>
-                        </FormGroup>
-                        <FormGroup >
-                            <Row>
-                                <Col sm={12}>
-                                    <Label htmlFor='email' sm={2}>인증번호</Label>
-                                    <span>&#10004; 인증완료</span>
-                                    <span>&#10060; 인증실패</span>
-                                    <Button style={{float:"right"}} outline color='secondary' size="sm" className="emailAuthBtn" onClick={(e)=>{e.preventDefault();}}>인증</Button>
-                                    <Input type='text' name='emailAuthNumber' id='emailAuthNumber' />&nbsp;
-                                </Col>
-                            </Row>
-                            <Row>
-                                <Col sm={12}>
-                                    <Label htmlFor='password' sm={2}>패스워드</Label>
-                                    <span>&#10060; 패스워드 양식에 어긋납니다</span>
-                                    <Input type='text' name='password' id='password' placeholder='특수문자, 대문자를 각각 1자 이상 포함한 12자리' />
-                                </Col>
-                                <Col sm={12}>
-                                    <Label htmlFor='password2' sm={2}>재입력</Label>
-                                    <span>&#10004; 패스워드 일치</span>
-                                    <span>&#10060; 패스워드 불일치</span>
-                                    <Input type='text' name='password2' id='password' />
-                                    
-                                </Col>
-                            </Row>
-                        </FormGroup>
-                        <FormGroup>
-                            
-                        </FormGroup>
-                    </Form>
+                <Form >
+                    <FormGroup >
+                        <Row>
+                            <Col>
+                                <Button color="success" onClick={context.loginShow == true ? context.signInterToggle : context.signUpToggle} style={{width:"285px"}}>네이버로 시작하기</Button>
+                            </Col>
+                        </Row>
+                    </FormGroup>
+                    <FormGroup >
+                        <Row>
+                            <Col>
+                                <Button color="warning" onClick={context.loginShow == true ? context.signInterToggle : context.signUpToggle} style={{width:"285px"}}>카카오로 시작하기</Button>
+                            </Col>
+                        </Row>
+                    </FormGroup>
+                </Form>
                 </ModalBody>
                 <ModalFooter >
-                    <Button color="secondary" onClick={context.loginShow == true ? context.signInterToggle : context.signUpToggle} style={{margin:"0 auto", width:"350px"}}>가입신청</Button>
+                    <Button outline color="secondary" onClick={context.loginShow == true ? context.signInterToggle : context.signUpToggle} style={{width:"285px"}}>ID / PW 회원가입</Button>
                 </ModalFooter>
             </Modal>
     )
