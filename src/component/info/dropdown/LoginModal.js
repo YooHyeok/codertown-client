e5da1fa4f8d54b9c29d17ef312e45f23ef567d35
webdevyoo <yjk7454@naver.com>
2023-08-17 09:35:53 +0900

    MOD: LoginModal CSS변경 - Form raw속성 제거 - Label과 Input 수직 출력

--- src/component/info/dropdown/LoginModal.js
+++ src/component/info/dropdown/LoginModal.js
@@ -6,8 +6,7 @@ import { Link } from 'react-router-dom';
 export default function LoginModal() {
     const context = useContext(HeaderLoginContext);
     const modalStyle = { 
-        width: "500px",
-        height: "500px",
+        width: "350px",
         top: "20%"
         // , left: "30%"
         // , position: "fixed"
@@ -18,24 +17,22 @@ export default function LoginModal() {
                 <ModalHeader toggle={context.loginToggle} >
                     <span style={{textAlign:"center !important"}}>로그인</span>
                 </ModalHeader>
-                <ModalBody>
-                    <Form style={{margin:"20px auto", marginBottom:"-40px", paddingLeft:"30px"}}>
-                        <FormGroup row style={{paddingLeft:"20px"}}>
-                            <Label htmlFor='email' sm={2}>이&nbsp;&nbsp;메&nbsp;&nbsp;일</Label>
-                            <Col sm={8}>
+                <ModalBody style={{width: "350px", height:"250px", margin:"0px auto"}}>
+                    <Form style={{marginBottom:"0px"}}>
+                        <FormGroup>
+                            <Label htmlFor='email' sm={2}>이메일</Label>
+                            <Col sm={12}>
                                 <Input type='text' name='email' id='email' />
                             </Col>
-                        </FormGroup>
-                        <FormGroup row style={{paddingLeft:"20px"}}>
                             <Label htmlFor='password' sm={2}>패스워드</Label>
-                            <Col sm={8}>
+                            <Col sm={12}>
                                 <Input type='text' name='password' id='password' />
                             </Col>
                         </FormGroup>
-                        <FormGroup row style={{paddingLeft:"20px"}}>
-                            <Col sm={10}>
+                        <FormGroup row>
+                            <Col sm={12}>
                                 <Input type='checkbox' name='rememberMe' id='rememberMe' /> &nbsp;Remember Me
-                                <div style={{width:"350px", position:"relative", top:"-20px"}}>
+                                <div style={{width:"310px", position:"relative", top:"-20px"}}>
                                     <Link style={{float:"right"}} onClick={(e)=>{e.preventDefault(); context.signUpToggle();}}>회원가입</Link> <br/>
                                     <Link style={{float:"right"}}>아이디/패스워드 찾기</Link>
                                 </div>
@@ -43,7 +40,7 @@ export default function LoginModal() {
                         </FormGroup>
                     </Form>
                 </ModalBody>
-                <ModalFooter style={{width:"500px"}}>
+                <ModalFooter>
                     <Button color="secondary" onClick={context.loginToggle} style={{margin:"0 auto", width:"350px"}}>로그인</Button>
                 </ModalFooter>
             </Modal>
