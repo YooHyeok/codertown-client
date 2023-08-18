22d8d4f6be18055d34cb1e016db99cceeec63647
webdevyoo <yjk7454@naver.com>
2023-08-18 09:50:01 +0900

    CREATE&ADD: MyPage 내정보수정 컴포넌트화

---
+++ src/component/mypage/MyInfoEdit.js
@@ -0,0 +1,95 @@
+import { useState, useEffect } from "react";
+import { Link } from 'react-router-dom';
+import { Form, Label, Input, Button, Col, FormGroup } from 'reactstrap';
+
+export default function MyInfoEdit() {
+
+    const [userin, setUserIn] = useState({
+        nickname: '', id: '', password: '', postcode: '', address: '', addrDetail: '', email: '', thumbnail: null
+    });
+
+    /**
+     * 기본 프로필 사진
+     */
+    const [src, setSrc] = useState('/default-profile.png');
+
+    /**
+     * 컴포넌트 생명주기 Hook
+     */
+    useEffect(() => {
+
+    }, [])
+
+    /**
+     * 첨부파일
+     */
+    const fileChange = (e) => {
+        setUserIn({ ...userin, thumbnail: e.target.files[0] })
+        console.log(e.value);
+        readImage(e.target);
+
+    }
+
+    /**
+     * 프로필 
+     */
+    const readImage = (input) => {
+        if (input.files && input.files[0]) {
+            const reader = new FileReader();
+            reader.onload = e => {
+                setSrc(e.target.result);
+            }
+            reader.readAsDataURL(input.files[0]);
+        }
+    }
+
+    /**
+     * JSX 시작
+     */
+    return (
+        <Form style={{ width: "400px", margin: '40px auto'}}>
+            {/* 프로필 */}
+            <div className="profile-wrap" style={{ marginBottom:"50px"}}>
+                <img className="profile" src={src} alt="profile"/>
+            </div>
+            <FormGroup row >
+                <Col sm={5}>
+                    <Input bsSize="sm" type="file" name="file" id="file" onChange={fileChange} accept='image/*' style={{ width: '400px', marginBottom:"25px"}}/>
+                </Col>
+            </FormGroup>
+            {/* 닉네임 */}
+            <FormGroup row>
+                <Label htmlFor='nickname' sm={4}>닉&nbsp;&nbsp;네&nbsp;&nbsp;임</Label>
+                <Col sm={5}>
+                    <Input type='text' name='nickname' id='nickname' value={""} onChange={(e)=>{e.preventDefault();}} required />
+                </Col>
+                <Col sm={3} >
+                    <Button outline color='secondary' style={{ width: '100%' }} onClick={(e)=>{e.preventDefault();}}>중복</Button>
+                </Col>
+                    <span id="regnnTrue" style={{ display: "none" }}><p><b>알맞은 형식입니다. 중복 확인을 해주세요.</b></p></span>
+                    <span id="regnnFalse" style={{ display: "none" }}><p><b>첫 글자는 영문자와 4~16자의 영문 대소문자, <br />숫자와 특수기호(_),(-)만 사용가능합니다.</b></p></span>
+            </FormGroup>
+            {/* 이메일 */}
+            <FormGroup row>
+                <Label htmlFor='email' sm={4}>이&nbsp;&nbsp;메&nbsp;&nbsp;일</Label>
+                <Col sm={8}>
+                    <Input type='email' name='email' id='email' value={""} onChange={(e)=>{e.preventDefault();}} required />
+                </Col>
+            </FormGroup>
+            {/* 패스워드 */}
+            <FormGroup row>
+                <Label htmlFor='grade' sm={4}>패스워드</Label>
+                <Col>
+                    <Button outline color='secondary' style={{ width: '100%', marginBottom:"25px" }} onClick={(e)=>{e.preventDefault();}}>재발급</Button>
+                </Col>
+            </FormGroup>
+            
+            {/* 수정 완료 버튼 */}
+            <FormGroup row>
+                <Col sm={5} >
+                    <Link to={'/mypage'}><Button color='secondary' style={{ width: '400px' }} onClick={(e)=>{e.preventDefault();}}>저장</Button></Link>
+                </Col>
+            </FormGroup>
+        </Form>
+    )
+}
--- src/component/mypage/MyPage.js
+++ src/component/mypage/MyPage.js
@@ -2,11 +2,10 @@
 import { useState, useEffect, useRef } from "react";
 import { Form, Label, Input, Button, Col, FormGroup, TabContent, TabPane, Nav, NavItem, NavLink, Table, Row} from 'reactstrap';
 import { BsFillSuitHeartFill } from "react-icons/bs"
-
 import classnames from 'classnames';
 import './MyPage.css';
-import { Link } from 'react-router-dom';
-import { useSelector } from 'react-redux'; // redux state값을 읽어온다. 토큰값과 userId값을 가져온다.
+import MyInfoEdit from './MyInfoEdit';
+
 
 export default function MyPage() {
     const divStyle = {
@@ -19,26 +18,7 @@ export default function MyPage() {
         , top: '100'
     };
     const selectRef = useRef(null);
-    const userId = useSelector((state) => { return state.UserId });
 
-    const [userin, setUserIn] = useState({
-        nickname: '', id: '', password: '', postcode: '', address: '', addrDetail: '', email: '', thumbnail: null
-    });
-
-    const [src, setSrc] = useState('/default-profile.png');
-    useEffect(() => {
-        /* axios.get('/mypage', { params: { id: userId } })
-            .then((res) => {
-                console.log(res);
-                console.log(res.data);
-                setUser(res.data);
-                if (res.data.filename != null) {
-                    setSrc("/profile/" + res.data.id);
-                }
-            }).catch((error) => {
-                console.log(error)
-            }) */
-    }, [])
 
     // Tab
     const [tabChange, setTabChange] = useState({activeTab:'1'});
@@ -50,22 +30,6 @@ export default function MyPage() {
         }
     }
 
-    // 첨부파일
-    const fileChange = (e) => {
-        setUserIn({ ...userin, thumbnail: e.target.files[0] })
-        console.log(e.value);
-        readImage(e.target);
-
-    }
-    const readImage = (input) => {
-        if (input.files && input.files[0]) {
-            const reader = new FileReader();
-            reader.onload = e => {
-                setSrc(e.target.result);
-            }
-            reader.readAsDataURL(input.files[0]);
-        }
-    }
 
 
     const [coggleList , setCoggleList] = useState([])
@@ -113,62 +77,14 @@ export default function MyPage() {
                 </div>
         {/* ============== 탭 컨텐츠 영역 ============== */}
                 <TabContent activeTab={tabChange.activeTab}>
-
                         <TabPane tabId="1">
-                            <Row>
-                            <Col sm="12">
                                 {/* 내정보 수정 시작 */}
-                                <Form style={{ width: "400px", margin: '40px auto'}}>
-                                    {/* 프로필 */}
-                                    <div className="profile-wrap" style={{ marginBottom:"50px"}}>
-                                        <img className="profile" src={src} alt="profile"/>
-                                    </div>
-                                    <FormGroup row >
-                                        <Col sm={5}>
-                                            <Input bsSize="sm" type="file" name="file" id="file" onChange={fileChange} accept='image/*' style={{ width: '400px', marginBottom:"25px"}}/>
-                                        </Col>
-                                    </FormGroup>
-                                    {/* 닉네임 */}
-                                    <FormGroup row>
-                                        <Label htmlFor='nickname' sm={4}>닉&nbsp;&nbsp;네&nbsp;&nbsp;임</Label>
-                                        <Col sm={5}>
-                                            <Input type='text' name='nickname' id='nickname' value={""} onChange={(e)=>{e.preventDefault();}} required />
-                                        </Col>
-                                        <Col sm={3} >
-                                            <Button outline color='secondary' style={{ width: '100%' }} onClick={(e)=>{e.preventDefault();}}>중복</Button>
-                                        </Col>
-                                            <span id="regnnTrue" style={{ display: "none" }}><p><b>알맞은 형식입니다. 중복 확인을 해주세요.</b></p></span>
-                                            <span id="regnnFalse" style={{ display: "none" }}><p><b>첫 글자는 영문자와 4~16자의 영문 대소문자, <br />숫자와 특수기호(_),(-)만 사용가능합니다.</b></p></span>
-                                    </FormGroup>
-                                    {/* 이메일 */}
-                                    <FormGroup row>
-                                        <Label htmlFor='email' sm={4}>이&nbsp;&nbsp;메&nbsp;&nbsp;일</Label>
-                                        <Col sm={8}>
-                                            <Input type='email' name='email' id='email' value={""} onChange={(e)=>{e.preventDefault();}} required />
-                                        </Col>
-                                    </FormGroup>
-                                    {/* 패스워드 */}
-                                    <FormGroup row>
-                                        <Label htmlFor='grade' sm={4}>패스워드</Label>
-                                        <Col>
-                                            <Button outline color='secondary' style={{ width: '100%', marginBottom:"25px" }} onClick={(e)=>{e.preventDefault();}}>재발급</Button>
-                                        </Col>
-                                    </FormGroup>
-                                    
-                                    {/* 수정 완료 버튼 */}
-                                    <FormGroup row>
-                                        <Col sm={5} >
-                                            <Link to={'/mypage'}><Button color='secondary' style={{ width: '400px' }} onClick={(e)=>{e.preventDefault();}}>저장</Button></Link>
-                                        </Col>
-                                    </FormGroup>
-                                </Form>
-                            </Col>
-                            </Row>
+                                <MyInfoEdit/>
                         </TabPane>
 
                         <TabPane tabId="2">
                             <Row>
-                            <Col sm="12">
+                            <Col>
                             <div style={{borderTop: '0.1px solid lightgray', margin:"30px auto"}}>
                                 <Table >
                                     <thead>
@@ -251,7 +167,7 @@ export default function MyPage() {
 
                         <TabPane tabId="3">
                             <Row>
-                            <Col sm="12">
+                            <Col>
                                 <div style={{borderTop: '0.1px solid lightgray', margin:"30px auto"}}>
                                     <Table >
                                         <thead>
