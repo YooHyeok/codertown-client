13b82a4c5b6dd2d543abc42ea1c032d401e8dd06
webdevyoo <yjk7454@naver.com>
2023-08-18 13:33:37 +0900

    CREAETE&ADD: ToastEditor 컴포넌트 추가 및 글쓰기 폼  적용

--- src/component/coggle/CoggleWrite.js
+++ src/component/coggle/CoggleWrite.js
@@ -1,17 +1,28 @@
 import { useNavigate } from 'react-router-dom';
 import { Button, Form, FormGroup, Label, Col, Input} from 'reactstrap';
+import { useState, createContext } from 'react';
+import ToastEditor from '../ToastEditor.js'
 
+export const Coggle = createContext();
 export default function CoggleWrite() {
     const divStyle = {
         width: '1200px' //캘린더 width 조절을 위해 부모태그에 설정한다.
-        , height: '900px'
+        , height: '100%'
         , textAlign: 'left'
-        , margin: '100px auto'
-        , marginBottom: '50px'
+        , margin: '50px auto'
+        , marginBottom: '150px'
         , padding: '30px'
         , top: '100'
       };
       const navigate = useNavigate();
+
+    const [toastHtml, setToastHtml] = useState('');
+    const [toastMarkdown, setMarkdown] = useState('');
+    const context = {
+        setToastHtml: setToastHtml.bind(this),
+        setMarkdown: setMarkdown.bind(this)
+    }
+
     return(
         <div style={divStyle}>
                 <div style = {{display:"flex"}}>
@@ -23,7 +34,7 @@ export default function CoggleWrite() {
                     </div>
                 </div>
                 <div style={{borderTop: '0.1px solid lightgray'}}>
-                    <Form style={{width:"800px", height:"700px", margin:"0px auto", margin:"50px 150px 50px 185px"}}>
+                    <Form style={{width:"824px", height:"700px", margin:"30px auto"}}>
                         <FormGroup row >
                             <Label style={{width:"95px"}} htmlFor='password' sm={2}>카테고리</Label>
                             <Col sm={1}>
@@ -38,24 +49,25 @@ export default function CoggleWrite() {
                         </FormGroup>
                         <FormGroup row >
                             <Label style={{width:"95px"}} htmlFor='email' sm={2}>제&nbsp;&nbsp;목</Label>
-                            <Col sm={10} style={{width:"660px"}}>
+                            <Col sm={7}>
                                 <Input type='text' name='email' id='email' />
                             </Col>
                         </FormGroup>
                         <FormGroup row>
                             <Label htmlFor='password2' sm={11}>내용 (에디터 교체 예정) </Label>
-                            <Col sm={11}>
-                                <Input type='textarea' name='password2' id='password' 
-                                style={{width:"730px", height:"500px", overflow: "auto"}}/>
+                            <Col>
+                                {/* <Input type='textarea' name='password2' id='password' 
+                                style={{width:"730px", height:"500px", overflow: "auto"}}/> */}
+                                <Coggle.Provider value={context} >
+                                    <ToastEditor/>
+                                </Coggle.Provider>
                                 <br/>
                                 <div style={{float:"right"}} >
                                 <Button color='secondary' outline onClick={(e)=>{e.preventDefault(); navigate(-1);}}>취소</Button>&nbsp;&nbsp;
                                 <Button color='secondary' onClick={(e)=>{e.preventDefault();}}>저장</Button>
                                 </div>
                             </Col>
-                            
                         </FormGroup>
-                        
                     </Form>
                 </div>
         </div>
