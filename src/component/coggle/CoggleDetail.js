4159a6ddd2f258f74c89dd795b3722bc3e28ebc7
webdevyoo <yjk7454@naver.com>
2023-08-25 12:17:05 +0900

    MOD: 코글 상세보기 호스트 통신 및 데이터 출력 완료

--- src/component/coggle/CoggleDetail.js
+++ src/component/coggle/CoggleDetail.js
@@ -3,6 +3,7 @@ import { useState, useEffect, useRef } from 'react';
 import axios from "axios";
 import { Viewer } from '@toast-ui/react-editor';
 import _ from 'lodash'; // Lodash 라이브러리
+import { Link, useParams, useNavigate } from 'react-router-dom';
 
 export default function CoggleDetail() {
     const divStyle = {
@@ -15,45 +16,39 @@ export default function CoggleDetail() {
         , top: '100'
       };
 
-    /* TextArea 개행 추가 및 제거 시 영역 확장 축소 */
+    /* 댓글영역 - TextArea 개행 추가 및 제거 시 영역 확장 축소 */
     const textarea = useRef('');
     const handleResizeHeight = () => {
     textarea.current.style.height = 'auto';
     textarea.current.style.height = textarea.current.scrollHeight + 'px';
     };
 
-    const [cokkiri, setCokkiri] = useState(
+    const { coggleNo } = useParams();
+
+    const [coggle, setCoggle] = useState(
         {   
             title: null,
             writer: {},
-            nickname: "fghij",
-            content: "updatevalue",
-            link: "updatevalue",
-            objectWeek: 4,
-            subject: "abcde",
-            teamName: "fghij",
-            projectParts: []
+            nickname: "",
+            category: '카카카',
+            content: "",
         }
              )
 
     useEffect(()=> {
-        axios.get('/cokkiri-detail/2')
+        axios.get(`/coggle-detail/${coggleNo}`)
         .then((response)=> {
-            console.log(response.data);
-            setCokkiri({...cokkiri,     
-                        title: response.data.cokkiriDto.title, 
-                        writer: response.data.cokkiriDto.writer,
-                        nickname: response.data.cokkiriDto.writer.nickname,
-                        content: response.data.cokkiriDto.content,
-                        link: response.data.cokkiriDto.link,
-                        objectWeek: response.data.cokkiriDto.objectWeek,
-                        subject: response.data.projectDto.subject,
-                        teamName: response.data.projectDto.teamName,
-                        projectParts: response.data.projectDto.projectParts
+            console.log(response.data.category)
+            setCoggle({  
+                        title: response.data.title, 
+                        writer: response.data.writer,
+                        nickname: response.data.writer.nickname,
+                        category : response.data.category,
+                        categoryText: response.data.category == 'T' ? 'TechQue' : response.data.category == 'C' ? 'Carrier' :  'DevLife',
+                        title: response.data.title,
+                        content: response.data.content,
                         }
-                                    
             )
-            console.log(cokkiri)
         })
         .catch((error) => {
             console.log(error);
@@ -65,16 +60,16 @@ export default function CoggleDetail() {
                 <div style = {{width:'1200px', margin: '0px auto', borderBottom: '0px solid lightgray'}}>
                     <div style={{width:'220px', margin: '0px auto', display:"flex"}}>
                         <h1 style={{margin:"30px auto"}}><b>코글</b></h1>
-                        <h6 style={{width:'130px',margin:"50px auto"}}>devlife</h6>
+                        <h6 style={{width:'130px',margin:"50px auto"}}>{coggle.categoryText}</h6>
                     </div>
                 </div>
                 {/* 글 제목 */}
                 <div style = {{width:'1200px', margin: '50px auto', display:"flex", borderBottom: '0.1px solid lightgray'}}>
                     <div style={{width:'1200px',margin:"30px 20px 10px 10px"}}>
-                        <h3 ><b>{cokkiri.title}</b></h3>
+                        <h3 ><b>{coggle.title}</b></h3>
                         <img src='/default_profile2.png' style={{width:'40px', height:'40px', margin:'5px', borderRadius:'50%', float:"left"}}/> 
                         <div>
-                            <span>{cokkiri.nickname}</span> <br/> <span>{'2023-08-21'}</span> <span>조회수 {'33'}</span>
+                            <span>{coggle.nickname}</span> <br/> <span>{'2023-08-21'}</span> <span>조회수 {'33'}</span>
                         </div>
                         <div className='update-delete-btn-gruop' style={{display:"flex", marginTop:"-40px", float:'right'}}>
                             <Button color='secondary'>수정</Button>&nbsp;&nbsp;
@@ -86,7 +81,7 @@ export default function CoggleDetail() {
                 <div style={{width:'1000px', height:'100%', margin :'0px auto'}}>
                     {/* 토스트 뷰어 영역 */}
                     <div style={{ width: '1000px', minHeight:'600px', margin:'10px'}}>
-                        <Viewer className="toast-viewer" initialValue={cokkiri.content} key={cokkiri.content}/>
+                        <Viewer className="toast-viewer" initialValue={coggle.content} key={coggle.content}/>
                     </div>
                     
                         {/* <Button color='secondary'>목록으로</Button> */}
