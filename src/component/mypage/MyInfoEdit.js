98a8c16aa2dacd1668e9f37c181eb8ad4e8ba8ee
webdevyoo <yjk7454@naver.com>
2023-08-30 13:51:20 +0900

    MOD: 마이페이지 내정보수정 구성 수정

--- src/component/mypage/MyInfoEdit.js
+++ src/component/mypage/MyInfoEdit.js
@@ -46,10 +46,10 @@ export default function MyInfoEdit() {
      * JSX 시작
      */
     return (
-        <Form style={{ width: "400px", margin: '40px auto'}}>
+        <Form style={{ width: "400px", margin: '30px auto'}}>
             {/* 프로필 */}
             <div className="profile-wrap" style={{ marginBottom:"50px"}}>
-                <img className="profile" src={src} alt="profile" style={{width:'100px', height:'100px', border:"0.1px solid lightgray "}}/>
+                <img className="profile" src={src} alt="profile"/>
             </div>
             <FormGroup row >
                 <Col sm={5}>
@@ -68,20 +68,43 @@ export default function MyInfoEdit() {
                     <span id="regnnTrue" style={{ display: "none" }}><p><b>알맞은 형식입니다. 중복 확인을 해주세요.</b></p></span>
                     <span id="regnnFalse" style={{ display: "none" }}><p><b>첫 글자는 영문자와 4~16자의 영문 대소문자, <br />숫자와 특수기호(_),(-)만 사용가능합니다.</b></p></span>
             </FormGroup>
-            {/* 이메일 */}
+            {/* 패스워드 */}
             <FormGroup row>
-                <Label htmlFor='email' sm={4}>이&nbsp;&nbsp;메&nbsp;&nbsp;일</Label>
+                <Label htmlFor='nickname' sm={4}>기존 패스워드</Label>
                 <Col sm={8}>
+                    <Input type='text' name='nickname' id='nickname' onChange={(e)=>{e.preventDefault();}} required />
+                </Col>
+            </FormGroup>
+            <FormGroup row>
+                <Label htmlFor='nickname' sm={4}>바꿀 패스워드</Label>
+                <Col sm={8}>
+                    <Input type='text' name='nickname' id='nickname' onChange={(e)=>{e.preventDefault();}} required />
+                </Col>
+            </FormGroup>
+            <FormGroup row>
+                <Label htmlFor='nickname' sm={4}>패스워드 확인</Label>
+                <Col sm={8}>
+                    <Input type='text' name='nickname' id='nickname' onChange={(e)=>{e.preventDefault();}} required />
+                </Col>
+            </FormGroup>
+            {/* 이메일 */}
+            {/* <FormGroup row>
+                <Col sm={12}>
+                    <Label htmlFor='email' style={{float:"left"}}>이&nbsp;&nbsp;메&nbsp;&nbsp;일</Label>
+                    <Button style={{float:"right"}} outline color='secondary' onClick={(e)=>{e.preventDefault();}}>중복</Button>
                     <Input type='email' name='email' id='email' value={""} onChange={(e)=>{e.preventDefault();}} required />
                 </Col>
             </FormGroup>
-            {/* 패스워드 */}
             <FormGroup row>
-                <Label htmlFor='grade' sm={4}>패스워드</Label>
-                <Col>
-                    <Button outline color='secondary' style={{ width: '100%', marginBottom:"25px" }} onClick={(e)=>{e.preventDefault();}}>재발급</Button>
+                <Label htmlFor='nickname' sm={4}>인증번호</Label>
+                <Col sm={5}>
+                    <Input type='text' name='nickname' id='nickname' value={""} onChange={(e)=>{e.preventDefault();}} required />
                 </Col>
-            </FormGroup>
+                <Col sm={3} >
+                    <Button outline color='secondary' style={{ width: '100%' }} onClick={(e)=>{e.preventDefault();}}>인증</Button>
+                </Col>
+            </FormGroup> */}
+            
             
             {/* 수정 완료 버튼 */}
             <FormGroup row>
