0510dfdbf84d6c886751c7fb99ef4bf9b1e7384b
webdevyoo <yjk7454@naver.com>
2023-09-05 12:29:38 +0900

    MOD: 채팅 텍스트아리아 줄개행시 채팅본문 영역 감소 및 텍스트아리아 영역 증가 등 동적 처리

--- src/component/DirectMessengerButton.js
+++ src/component/DirectMessengerButton.js
@@ -1,10 +1,9 @@
 import { Button } from 'reactstrap';
 import { Messenger } from 'react-bootstrap-icons';
 import "react-chat-elements/dist/main.css"
-import { MessageBox } from "react-chat-elements";
-import { ChatList } from "react-chat-elements"
+import { MessageBox, ChatList, ChatItem, Input } from "react-chat-elements";
 
-import { useRef, useState } from 'react';
+import { useRef, useState, useEffect} from 'react';
 
 // npm install react-chat-elements --save --force
 
@@ -12,35 +11,38 @@ export default function DirectMessengerButton() {
 
     const textareaRef = useRef('');
     const chatContainerRef = useRef('');
-    const [textareValue, setTextareaValue] = useState('')
+    const [textareaValue, setTextareaValue] = useState('')
 
 
-
+    useEffect(() => {
+      if (textareaRef.current) {
+        textareaRef.current.scrollTop = textareaRef.current.scrollHeight;
+      }
+    }, [textareaValue]);
 
   /* 댓글 [입력] Change 이벤트 메소드 */
     const textAreaInputChange = (e) => {
         setTextareaValue(e.target.value)
-        
-    }
-
-    const textAreaInputKeyDown= (e) => {
-      if (e.target.value == '') {
-        textareaRef.current.style.height = '30px';
-        textareaRef.current.value = null;
-        chatContainerRef.current.style.height = '384px';
-        return;
-      }
-      if (e.key === 'Enter' && e.shiftKey) {
-        // Shift + Enter를 눌렀을 때 줄개행
-        setTextareaValue(e.target.value + '\n');
-        textareaRef.current.style.height = 'auto';
-        textareaRef.current.style.height = textareaRef.current.scrollHeight + 'px';
-        console.log(textareaRef.current.style.height)
-        if (textareaRef.current.style.height != '28px') {
-          chatContainerRef.current.style.height = 'auto';
-          chatContainerRef.current.style.height = 'calc(384px - ' + textareaRef.current.style.height  +  ')';
+        if (e.target.value == '') {
+          textareaRef.current.style.height = '30px';
+          textareaRef.current.value = null;
+          chatContainerRef.current.style.height = '380px';
+          return;
         }
+    }
+    const textAreaInputKeyDown= (e) => {
+      
+      if (e.key === 'Enter' && e.shiftKey) {
         e.preventDefault(); // 엔터 키의 기본 동작 방지
+        // Shift + Enter를 눌렀을 때 줄개행
+        setTextareaValue(textareaValue + '\n');
+        textareaRef.current.style.overflow = 'auto';
+        textareaRef.current.style.height = 'auto';
+        console.log(textareaRef.current.scrollHeight)
+        textareaRef.current.style.height = (30+textareaRef.current.scrollHeight) + 'px';
+        // textareaRef.current.focus();
+        chatContainerRef.current.style.height = 'auto';
+        chatContainerRef.current.style.height = `calc(408px - ${textareaRef.current.scrollHeight}px)`;
         return;
       } 
       
@@ -48,19 +50,14 @@ export default function DirectMessengerButton() {
         e.preventDefault(); // 엔터 키의 기본 동작 방지
         // 여기에 메시지 전송 로직 추가
         // ...
-        console.log(textareValue)
+        console.log(textareaValue)
         // 메시지 전송 후 입력창 초기화
-        // setTextareaValue('');
+        setTextareaValue('');
+        chatContainerRef.current.style.height = '380px';
         textareaRef.current.style.height = '30px'; // 초기 높이로 설정
         return;
       }
       
-     /*  if (e.target.value == '') {
-        textareaRef.current.style.height = '30px';
-        textareaRef.current.value = null;
-        chatContainerRef.current.style.height = '384px';
-        return;
-      } */
     }
 
     const dmButtonStyle = {
@@ -89,6 +86,7 @@ export default function DirectMessengerButton() {
         , zIndex: '10'
         , bottom: "216px"
         , right: "100px"
+        , textAlign:'left'
         , height: "calc(100% - 116px) !important"
       };
 
@@ -165,19 +163,26 @@ export default function DirectMessengerButton() {
                 </div>
                 {/* 채팅 컴포넌트 */}
                 <div style={dmFrameStyle}>
-                    {/* <iframe style={{position:"relative!important", height:"100%!important", width:"100%!important", border:"none!important"}}>
-asdasdasdasd
-                    </iframe> */}
                     {/* 채팅 리스트 컴포넌트 */}
                     <div className="chat-list-div" style={{width:'452px', height:'506px', backgroundColor:'white', border : "1px solid lightgray", overflow:'auto', textAlign:'left'}}>
                       <ChatList className='chat-list' dataSource={dataSource} />
                     </div>
+
                     {/* 채팅방 입장 컴포넌트 */}
                     <div className="chat-into" style={{width:'450px', height:'506px', backgroundColor:'white', border : "1px solid lightgray"}}>
                       {/* 채팅방 제목 영역*/}
-                      <div className="chat-into-header" style={{width:'448px', height:'70px', backgroundColor:'white', borderBottom : "1px solid lightgray"}}></div>
+                      <div className="chat-into-header" style={{width:'448px', height:'73px', backgroundColor:'white', borderBottom : "1px solid lightgray"}}>
+                        <ChatItem
+                            avatar="https://avatars.githubusercontent.com/u/80540635?v=4"
+                            alt="kursat_avatar"
+                            title="Kursat"
+                            subtitle=""
+                            date={new Date()}
+                            unread={0}
+                          />;
+                      </div>
                       {/* 채팅방 대화내용 리스트 영역 */}
-                      <div ref={chatContainerRef} className="chat-into-body" style={{width:'448px', height:'384px', minHeight:'270px', overflow:'auto', backgroundColor:'white', borderBottom : "1px solid lightgray"}}>
+                      <div ref={chatContainerRef} className="chat-into-body" style={{width:'448px', height:'380px', minHeight:'296px', overflow:'auto', backgroundColor:'white', borderBottom : "1px solid lightgray"}}>
                         {/* 상대방 메시지박스 */}
                         <MessageBox position='left'  title='Burhan'  type='text'  text="Hi there !"  date={new Date()}  replyButton={true}/>
                         {/* 메시지박스 */}
@@ -192,12 +197,13 @@ asdasdasdasd
                         <MessageBox position="right" title="Emre" type="text" text="Click to join the meeting" date={new Date()} />
                       </div>
                       {/* 채팅방 대화 전송 영역 */}
-                      <div className="chat-into-footer" style={{width:'448px', minHeight:'50px', overflow:'hidden', backgroundColor:'white', textAlign:'left', borderBottom : "1px solid lightgray"}}>
-                        <div style={{width:"400px", minHeight:'30px', margin:'0 auto', maxHeight:"114px !important"}}>
-                            <textarea rows={1} ref={textareaRef} onChange={textAreaInputChange} onKeyDown={(e)=>{
+                      <div className="chat-into-footer" style={{width:'448px', minHeight:'48px', overflow:'hidden', backgroundColor:'white'/* , borderBottom : "1px solid lightgray" */}}>
+                        <div style={{width:"400px", minHeight:'30px', margin:'0 auto', maxHeight:"112px !important"}}>
+                        {/* <Input placeholder="Type here..." multiline={true} /> */}
+                            {<textarea rows={1} ref={textareaRef} onChange={textAreaInputChange} onKeyDown={(e)=>{
                               textAreaInputKeyDown(e)
-                            }} value={textareValue}
-                                  style={{float:'left', display:'flex', width:'360px', height:'30px', maxHeight:"114px", margin:"10px auto", resize: 'none', outlineStyle:'none', overflow:'hidden', border: '0.1px solid lightgray'}} placeholder='내용을 입력하세요'></textarea>
+                            }} value={textareaValue}
+                                  style={{float:'left', display:'flex', width:'360px', height:'30px', maxHeight:"114px", margin:"10px auto", resize: 'none', outlineStyle:'none', overflow:'hidden', border: '0.1px solid lightgray'}} placeholder='내용을 입력하세요'></textarea>}
                         <div>
                           <Button size={'sm'} style={{float:'right', margin:'10px auto', background:"linear-gradient(rgb(104, 97, 236) 0%, rgb(127, 97, 236) 100%)"}}>전송</Button>
                         </div>
