8ae3e5db341e5e51ad8e6c635fa595f7f96d6b16
webdevyoo <yjk7454@naver.com>
2023-09-14 12:39:25 +0900

    MOD: 댓글 토탈 카운트 출력

--- src/component/coggle/CoggleDetail.js
+++ src/component/coggle/CoggleDetail.js
@@ -78,13 +78,23 @@ export default function CoggleDetail() {
         })
     };
 
+    
+    const [commentList, setCommentList] = useState([])
+    const [commentTotalCount, setCommentTotalCount] = useState([])
+
     /**
      * 댓글 조회 메소드
      */
     const commentSearchAxios = () => {
         axios.get(`/coggle/${coggleNo}/comment`)
         .then((response)=> {
+            /* 댓글 초기화 */
             setCommentList(response.data)
+
+            /* 댓글 토탈 카운트 초기화 */
+            var childCommnetSumCount = 0;
+            response.data.forEach(obj => childCommnetSumCount += obj.children.length)
+            setCommentTotalCount(response.data.length+childCommnetSumCount);
         })
         .catch((error) => {
             console.log(error);
@@ -108,7 +118,6 @@ export default function CoggleDetail() {
         }
              )
 
-    const [commentList, setCommentList] = useState([])
 
     useEffect(()=> {
         axios.get(`/coggle-detail/${coggleNo}/${userId == '' ? null : userId}`)
@@ -184,9 +193,20 @@ export default function CoggleDetail() {
                     <Viewer className="toast-viewer" initialValue={coggle.content} key={coggle.content}/>
                 </div>
             </div>
+            {/* 좋아요기능 & 댓글카운트  */}
             <div style = {{width:'1200px', margin: '0px auto', display:"flex"}}>
-                <div onClick={toggle} ><LikeButton coggleNo={coggleNo} isLikeMarked={coggle.isLikeMarked} className='inline' /></div>
-                &nbsp;{coggle.isLikedMarkedCount}
+                <div style = {{width:'50px', margin: '0px', display:"flex"}}>
+                    <div onClick={toggle} ><LikeButton coggleNo={coggleNo} isLikeMarked={coggle.isLikeMarked} className='inline' /></div>
+                    &nbsp;{coggle.isLikedMarkedCount}
+                </div>
+                <div style = {{width:'50px', margin: '0px', display:"flex"}}>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-chat-text" viewBox="0 0 16 16">
+                        <path d="M2.678 11.894a1 1 0 0 1 .287.801 10.97 10.97 0 0 1-.398 2c1.395-.323 2.247-.697 2.634-.893a1 1 0 0 1 .71-.074A8.06 8.06 0 0 0 8 14c3.996 0 7-2.807 7-6 0-3.192-3.004-6-7-6S1 4.808 1 8c0 1.468.617 2.83 1.678 3.894zm-.493 3.905a21.682 21.682 0 0 1-.713.129c-.2.032-.352-.176-.273-.362a9.68 9.68 0 0 0 .244-.637l.003-.01c.248-.72.45-1.548.524-2.319C.743 11.37 0 9.76 0 8c0-3.866 3.582-7 8-7s8 3.134 8 7-3.582 7-8 7a9.06 9.06 0 0 1-2.347-.306c-.52.263-1.639.742-3.468 1.105z"/>
+                        <path d="M4 5.5a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5zM4 8a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7A.5.5 0 0 1 4 8zm0 2.5a.5.5 0 0 1 .5-.5h4a.5.5 0 0 1 0 1h-4a.5.5 0 0 1-.5-.5z"/>
+                    </svg>
+                    &nbsp;{commentTotalCount}
+                    
+                </div>
             </div>
             {/* 댓글 영역 */}
             <div style = {{width:'1200px', margin: '30px auto', borderTop: '0.1px solid lightgray'}}>
