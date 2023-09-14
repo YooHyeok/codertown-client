4c9af08f1f92fbe3b75fc9162e025e0e51fdf0ad
webdevyoo <yjk7454@naver.com>
2023-09-14 10:16:28 +0900

    MOD: 맘모스/코끼리 메인 카드리스트 좋아요버튼 하트에서 북마크아이콘으로 변경 반영

--- src/component/main/MammothCardList.js
+++ src/component/main/MammothCardList.js
@@ -1,18 +1,20 @@
 import axios from "axios";
 import MammothCard from './MammothCard';
 import { useState, useEffect } from "react";
+import { useSelector } from 'react-redux'; // redux state값을 읽어온다 토큰값과 userId값을 가져온다.
 
 export default function MammothCardList() {
 
     const [mammothCardList , setMammothCardList] = useState([]);
     const [keyword , setKeyword] = useState('')
-      
+    const userId = useSelector((state) => { return state.UserId });
+
     /**
      * 코끼리 목록 출력 - 호스트 서버 통신 메소드
      * @param {} page : 선택된 페이지 정보 파라미터
      */
     const serverRequest = (page, keyword) => {
-        axios.get(`/recruit?page=${page}&size=${20}&dType=Mammoth&keyword=${keyword}`)
+        axios.get(`/recruit?page=${page}&size=${20}&dType=Mammoth&keyword=${keyword}&loginId=${userId}&url=${'main'}`)
         .then((response)=> {
             setMammothCardList(response.data.recruitList)
         })
