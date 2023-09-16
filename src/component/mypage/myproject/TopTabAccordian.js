cea5498ed77baec80c980a48fecd5bd86a410ca2
webdevyoo <yjk7454@naver.com>
2023-09-16 15:54:04 +0900

    MOD: 아코디언 Controlled 변경

--- src/component/mypage/myproject/TopTabAccordian.js
+++ src/component/mypage/myproject/TopTabAccordian.js
@@ -1,51 +1,8 @@
 import { useState } from "react";
 import { Table } from 'reactstrap';
-import { styled } from '@mui/material/styles';
-import ArrowForwardIosSharpIcon from '@mui/icons-material/ArrowForwardIosSharp';
-import MuiAccordion from '@mui/material/Accordion';
-import MuiAccordionSummary from '@mui/material/AccordionSummary';
-import MuiAccordionDetails from '@mui/material/AccordionDetails';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import { Accordion, AccordionDetails, AccordionSummary, Typography } from '@mui/material';
 
-import { /* MUI - Accordion */
-        Tabs, Tab, Box, AppBar /* MUI - Tabs */
-        , Typography, useTheme} from '@mui/material';
-
-const Accordion = styled((props) => (
-    <MuiAccordion disableGutters elevation={0} square {...props} />
-  ))(({ theme }) => ({
-    border: `1px solid ${theme.palette.divider}`,
-    '&:not(:last-child)': {
-      borderBottom: 0,
-    },
-    '&:before': {
-      display: 'none',
-    },
-  }));
-  
-  const AccordionSummary = styled((props) => (
-    <MuiAccordionSummary
-      expandIcon={<ArrowForwardIosSharpIcon sx={{ fontSize: '0.9rem' }} />}
-      {...props}
-    />
-  ))(({ theme }) => ({
-    backgroundColor:
-      theme.palette.mode === 'dark'
-        ? 'rgba(255, 255, 255, .05)'
-        : 'rgba(0, 0, 0, .03)',
-    flexDirection: 'row-reverse',
-    '& .MuiAccordionSummary-expandIconWrapper.Mui-expanded': {
-      transform: 'rotate(90deg)',
-    },
-    '& .MuiAccordionSummary-content': {
-      marginLeft: theme.spacing(1),
-    },
-  }));
-  
-  const AccordionDetails = styled(MuiAccordionDetails)(({ theme }) => ({
-    padding: theme.spacing(2),
-    borderTop: '1px solid rgba(0, 0, 0, .125)',
-  }));
-  
   export default function TopTabAccordian() {
     const [expanded, setExpanded] = useState('panel1');
   
@@ -56,8 +13,8 @@ const Accordion = styled((props) => (
     return (
       <div>
         <Accordion expanded={expanded === 'panel1'} onChange={handleChange('panel1')}>
-          <AccordionSummary aria-controls="panel1d-content" id="panel1d-header">
-            <Typography><h3>프로젝트 총 인원 현황</h3></Typography>
+          <AccordionSummary expandIcon={<ExpandMoreIcon />} aria-controls="panel1d-content" id="panel1d-header">
+            <Typography><h4>프로젝트 총 인원 현황</h4></Typography>
           </AccordionSummary>
           <AccordionDetails>
             <Typography>
@@ -107,8 +64,8 @@ const Accordion = styled((props) => (
           </AccordionDetails>
         </Accordion>
         <Accordion expanded={expanded === 'panel2'} onChange={handleChange('panel2')}>
-          <AccordionSummary aria-controls="panel2d-content" id="panel2d-header">
-            <Typography><h3>프로젝트 참여자 전체 리스트</h3></Typography>
+          <AccordionSummary expandIcon={<ExpandMoreIcon />} aria-controls="panel2d-content" id="panel2d-header">
+            <Typography><h4>프로젝트 참여자 전체 리스트</h4></Typography>
           </AccordionSummary>
           <AccordionDetails>
             <Typography>
