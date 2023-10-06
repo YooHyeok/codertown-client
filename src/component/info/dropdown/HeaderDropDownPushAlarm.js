c2b8e3607f055973acdd0aee7feaefc34e4dd4c7
webdevyoo <yjk7454@naver.com>
2023-10-06 19:43:37 +0900

    MOD: NOTIFICATION 클릭시 전체 갯수 초기화 로직 추가 및 아이콘 translate scale시 드롭다운메뉴 밑으로내려가는 현상 해결

--- src/component/info/dropdown/HeaderDropDownPushAlarm.js
+++ src/component/info/dropdown/HeaderDropDownPushAlarm.js
@@ -10,7 +10,7 @@ import * as  DateUtil from '../../../util/DateUtil.js'
 export default function HeaderDropDownPushAlarm() {
   const context = useContext(HeaderDropDownContext);
   const [pushAlarmList , setPushAlarmList] = useState([])
-  const [newNotifyCount, setNewNotifyCount] = useState(15);
+  const [newNotifyCount, setNewNotifyCount] = useState(0);
 
   const userId = useSelector( (state) => {return state.UserId} );
 
@@ -28,6 +28,19 @@ export default function HeaderDropDownPushAlarm() {
     })
     
   }, [])
+  useEffect(() => {
+    if(context.dropdownOpenPushAlarm === true){
+      const formdata = new FormData();
+      formdata.append("loginEmail", userId);
+      axios.post("/init-new-notify-count", formdata)
+        .then((response) => {
+          setNewNotifyCount(0)
+        })
+        .catch((error)=>{
+        })
+    }
+    
+  }, [context.dropdownOpenPushAlarm])
 
   const pushAlarmListRender = () => {
     return(
@@ -35,24 +48,21 @@ export default function HeaderDropDownPushAlarm() {
               return (
                 <Link key={index} style={{height:'98px !important', textDecoration:'none'}}>
                   {index != 0 && <DropdownItem divider />} {/* 첫번째 인자가 아닌 경우 출력 */}
-                <DropdownItem style={{width:'100%', lineHeight: "25px"}}>
+                <DropdownItem style={{width:'100%', lineHeight: "25px", backgroundColor: obj.isClicked ?  'white': '#f5f5f5'}}>
                   <div style={{width:'320px', height:'75px'}}>
                         <div style={{width:'50px', height:'75px', float:"left"}}>
                             <img src={`data:image/png;base64,${obj.profileUrl}`} style={{width:'40px', height:'40px', margin:'5px', borderRadius:'50%'}}/> 
                         </div>
                         <div style={{width:'260px', minheight:'75px', float:'right'}}>
                             <div style={{width:'180px', minheight:'75px', float:"left"}}>
-                              {/* 조건 */}
                               {obj.replyCondition === 'MYPOST' ? <span style={{display:'block', width:'180px', whiteSpace: 'pre-wrap' }}><b>{obj.writerNickname}</b>님이 댓글을 남겼습니다.</span>
                               :obj.replyCondition === 'MYMENTION' ? <span style={{display:'block', width:'180px', whiteSpace: 'pre-wrap' }}><b>{obj.writerNickname}</b>님이 댓글을 남겼습니다.</span>
                               : <span style={{display:'block', width:'180px', whiteSpace: 'pre-wrap' }}><b>{obj.writerNickname}</b>님이 <b>{obj.mentionNickname}</b>님에게 댓글을 남겼습니다.</span>}
-                              {/* 내용,제목 */}
                               <div style={{width:'180px'}}>
                                 <div><span style={{textOverflow: 'ellipsis'}}>{obj.commentContent}</span></div>
                                 <div><span style={{textOverflow: 'ellipsis'}}>{obj.coggleTitle}</span></div>
                               </div>
                             </div>
-                            {/* 날짜 */}
                       <div style={{width:'70px', minheight:'75px', heigt:'100%', float:"right"}}>
                             <span>{DateUtil.utcToKrYMD(obj.firstRegDate)}</span>
                       </div>
@@ -68,14 +78,15 @@ export default function HeaderDropDownPushAlarm() {
 
   return (
     <Dropdown id="ok" isOpen={context.dropdownOpenPushAlarm} fade="true" toggle={context.togglePushAlarm}>
-      <DropdownToggle caret style={{ backgroundColor: "rgba(0,0,0,0)", border: "none", height:"79px"}}>{/* 드롭다운 버튼 투명 처리*/}
+      <DropdownToggle caret style={{ backgroundColor: "rgba(0,0,0,0)", border: "none"}}>{/* 드롭다운 버튼 투명 처리*/}
         <Lightbulb  size={30} style={{color:"black"}}/>
-        {newNotifyCount > 0 && 
-        <span style={{backgroundColor: '#fa3e3e', borderRadius: '50%', color: 'white', padding: '1px 8px', fontSize: '15px', position: 'absolute', bottom: '42px', left: '25px'}}>
+
+      </DropdownToggle>
+      {newNotifyCount > 0 && 
+        <span style={{lineHeight:'25px',backgroundColor: '#fa3e3e', borderRadius: '50%', color: 'white', padding: '1px 8px', fontSize: '15px', position: 'absolute', bottom: '42px', left: '25px'}}>
             { newNotifyCount }
         </span>
         }
-      </DropdownToggle>
       <DropdownMenu style={{width:"365px", height:"495px"}}>
         {/* 드롭다운 헤더 */}
         <Link>
